ae86ce1f0cb335eabb4156526ad5658c33157cad
Jonathan Schoonbroodt <hello@johndoesit.be>
2024-04-21 01:08:24 +0200

    Update fuckads.js

--- fuckads.js
+++ fuckads.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         FuckAds - Skip YouTube ads
 // @namespace    http://tampermonkey.net/
-// @version      1.2
-// @description  Automatically skip YouTube ads.
+// @version      1.3
+// @description  Automatically skip (most of) YouTube ads and hide YouTube Ad Slots.
 // @author       John Doe
 // @match        *://www.youtube.com/*
 // @grant        none
@@ -12,6 +12,22 @@
 // ==/UserScript==
 
 (function () {
+  function hideAdsSlot () {
+    const youTubeAdsSlot = document.querySelectorAll('ytd-ad-slot-renderer')
+
+    if (youTubeAdsSlot) {
+      for (let i = 0; i < youTubeAdsSlot.length; i++) {
+        youTubeAdsSlot[i].innerHTML = 'ADS HIDDEN BY FUCK ADS USER SCRIPT'
+        youTubeAdsSlot[i].style.height = '100%'
+        youTubeAdsSlot[i].style.display = 'flex'
+        youTubeAdsSlot[i].style.color = 'white'
+        youTubeAdsSlot[i].style.justifyContent = 'center';
+        youTubeAdsSlot[i].style.alignItems = 'center';
+      }
+    }
+  }
+  setInterval(hideAdsSlot, 3000)
+
   function startObserving () {
     if (location.href.includes('/watch')) {
       const player = document.getElementById('movie_player')
